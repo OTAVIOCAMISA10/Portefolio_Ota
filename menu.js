2fa51976c43e90ec142bc63c3d680f973721f291
Otávio Augusto <133927895+OTAVIOCAMISA10@users.noreply.github.com>
2025-08-26 09:03:09 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,15 +1,20 @@
-let btnMenu = document.getElementById('btn-menu')
-let menu = document.getElementById('menu-mobile')
-let overlay = document.getElementById('overlay-menu')
+const btnMenu = document.getElementById('btnMenu');
+const menu = document.getElementById('menu');
+const overlay = document.getElementById('overlay');
 
-btnMenu.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
+if (btnMenu && menu && overlay) {
+  btnMenu.addEventListener('click', () => {
+    menu.classList.add('abrir-menu');
+  });
 
-menu.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+  const btnFechar = document.querySelector('.menu-mobile .btn-fechar');
+  if (btnFechar) {
+    btnFechar.addEventListener('click', () => {
+      menu.classList.remove('abrir-menu');
+    });
+  }
 
-overlay.addEventListener('click', ()=>{
-    menu.classList.remove('abrir-menu')
-})
+  overlay.addEventListener('click', () => {
+    menu.classList.remove('abrir-menu');
+  });
+}
